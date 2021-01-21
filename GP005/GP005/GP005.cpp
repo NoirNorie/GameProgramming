553bc600a435e48b4fa951bcb7b22f06c2f22652
LEE MYEONG JIN <lmjking1@gmail.com>
2021-01-21 23:57:39 +0900

    함수 템플릿
    GP005
    함수 템플릿

---
+++ GP005/GP005/GP005.cpp
@@ -0,0 +1,18 @@
+﻿// GP005.cpp : 함수 템플릿
+
+#include "stdafx.h"
+
+template <typename T>
+T maximum(T a, T b)
+{
+    return (a > b ? a : b);
+}
+
+int main()
+{
+    cout << "maximum(5, 3) = " << maximum(5, 3) << endl;
+    cout << "maximum('x', 'y') = " << maximum('x', 'y') << endl;
+    cout << "maximum(3.3, 5.5) = " << maximum(3.3, 5.5) << endl;
+
+    return 0;
+}
