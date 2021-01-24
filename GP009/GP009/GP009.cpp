fa266a1a2b294e01e4988f1dfa2405688889fdc3
LEE MYEONG JIN <lmjking1@gmail.com>
2021-01-25 03:23:15 +0900

    STL - Vector
    GP009
    
    STL - Vector

---
+++ GP009/GP009/GP009.cpp
@@ -0,0 +1,54 @@
+﻿// GP009.cpp : STL01
+//
+
+#include "stdafx.h"
+#define SIZE 10 // 정적 배열
+int S_arr[SIZE];
+
+int main()
+{
+    int size = 10;
+    vector<int> arr(size); // 크기 10의 벡터 선언
+    int* d_arr = new int[size]; // 동적 배열
+
+    for (int i = 0; i < size; i++) // 벡터
+    {
+        arr[i] = i;
+    }
+
+    for (int i = 0; i < size; i++) // 정적 배열
+    {
+        S_arr[i] = i;
+    }
+
+    for (int i = 0; i < size; i++) // 동적 배열
+    {
+        d_arr[i] = i;
+    }
+
+    cout << "벡터 출력" << endl;
+    for (int i = 0; i < size; i++) // 벡터 출력
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "정적 배열 출력" << endl;
+
+    for (int i = 0; i < size; i++) // 정적 배열 출력
+    {
+        cout << S_arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "동적 배열 출력" << endl;
+
+    for (int i = 0; i < size; i++) // 동적 배열 출력
+    {
+        cout << d_arr[i] << " ";
+    }
+
+    delete[] d_arr;
+
+    return 0;
+}
